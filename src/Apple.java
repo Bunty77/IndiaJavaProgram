52379275dd4989c2d03c82aa2ce630496e59987a
Navnath Sujgure <nso4579@MLINPU1502-0035.local>
2019-04-10 15:48:58 +0530

    new commit for india program

---
+++ src/Apple.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Apple {
+	
+	
+	public static void main(String args[])
+	{
+		String io = "absdk345fbskjdbfskdjbf";
+		String kk = io.replaceAll("([a-z])", "");
+		System.out.println("New String is:"+kk);
+		System.out.println("Welcome To Docker By Navnath");
+		
+	}
+
+}
